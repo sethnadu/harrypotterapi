bad076ab1cce8e3e7fe1e3744be6fc1977fb4e9b
Seth Nadu <seth.nadu@gmail.com>
2019-08-04 22:13:40 -0400

    working on routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 
 
 import AppRouter from "./Components/AppRouter.js"
+import CharacterList from "./Components/CharacterList";
+import SpellsList from "./Components/SpellsList.js";
 import Nav from "./Components/Nav.js";
 
 import './App.css';
@@ -15,8 +17,9 @@ function App() {
     <Router>
     <div className="App">
       <Nav />
-      <AppRouter />
     <Route exact path="/" />
+    <Route path="/names" component ={AppRouter} />
+    <Route path="/spells" component ={AppRouter} />
    
     </div>
     </Router>
